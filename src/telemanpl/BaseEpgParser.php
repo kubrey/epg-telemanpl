075f76f4c7392b210cf2c44512181da468a53442
kubrey <kubrey@gmail.com>
2016-05-06 16:03:00 +0300

    basic functionality

---
+++ src/telemanpl/BaseEpgParser.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kubrey
+ * Date: 06.05.16
+ * Time: 14:55
+ */
+
+namespace telemanpl;
+
+
+class BaseEpgParser
+{
+    protected $config = array(
+        'baseUrl' => 'http://www.teleman.pl/program-tv/stacje/',
+        'curlProxy' => false,//false or ip
+        'curlTor' => false,
+        'channelsUrl' => 'http://www.teleman.pl/moje-stacje',
+        'curlTorPort' => null //set if curlTor is true(default 9050)
+    );
+
+    protected $curlOptions = array();
+    protected $curlError = null;
+    protected $curlResult = null;
+    protected $curlInfo = array();
+    protected $errors = array();
+    protected $curlObject = null;
+
+    /**
+     * @param array $config
+     */
+    public function __construct($config = array()) {
+        if ($config) {
+            foreach ($config as $key => $value) {
+                $this->config[$key] = $value;
+            }
+        }
+    }
+
+    /**
+     * @param mixed $key
+     * @param mixed $val
+     * @return $this
+     */
+    public function setCurlOption($key, $val) {
+        $this->curlOptions[$key] = $val;
+        return $this;
+    }
+
+    /**
+     * @param string $url
+     * @return $this
+     */
+    protected function initCurl($url) {
+        $this->resetCurl();
+        if (!$this->curlOptions) {
+            $this->curlOptions[CURLOPT_USERAGENT] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36";
+            $this->curlOptions[CURLOPT_TIMEOUT] = 60;
+            $cookie = "epg_cookie.txt";
+            $this->curlOptions[CURLOPT_COOKIEJAR] = $cookie;
+            $this->curlOptions[CURLOPT_COOKIE] = $cookie;
+            $this->curlOptions[CURLOPT_FOLLOWLOCATION] = 1;
+            $this->curlOptions[CURLOPT_RETURNTRANSFER] = 1;
+        }
+        $this->curlOptions[CURLOPT_URL] = $url;
+        if ($this->config['curlProxy']) {
+            $this->curlOptions[CURLOPT_PROXY] = $this->config['curlProxy'];
+        } elseif ($this->config['curlTor']) {
+            $this->setCurlTor();
+        }
+        if (!$this->curlObject) {
+            $this->curlObject = curl_init($url);
+        }
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    protected function resetCurl() {
+        $this->curlObject = null;
+        $this->curlOptions = array();
+        $this->curlInfo = array();
+        $this->curlResult = null;
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    protected function runCurl() {
+        try {
+            curl_setopt_array($this->curlObject, $this->curlOptions);
+            $this->curlResult = curl_exec($this->curlObject);
+            $this->curlError = curl_error($this->curlObject);
+            $this->curlInfo = curl_getinfo($this->curlObject);
+            curl_close($this->curlObject);
+        } catch (\Exception $e) {
+            $this->setError($e->getMessage());
+            $this->curlError = $e->getMessage();
+        }
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    protected function setCurlTor() {
+        $this->curlOptions[CURLOPT_AUTOREFERER] = 1;
+        $this->curlOptions[CURLOPT_RETURNTRANSFER] = 1;
+        $this->curlOptions[CURLOPT_PROXY] = '127.0.0.1:' . ($this->config['curlTorPort'] ? (int)$this->config['curlTorPort'] : 9050);
+        $this->curlOptions[CURLOPT_PROXYTYPE] = 7;
+        $this->curlOptions[CURLOPT_TIMEOUT] = 120;
+        $this->curlOptions[CURLOPT_VERBOSE] = 0;
+        $this->curlOptions[CURLOPT_HEADER] = 0;
+        return $this;
+    }
+
+    /**
+     * @param $error
+     * @return $this
+     */
+    public function setError($error) {
+        $this->errors[] = $error;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCurlInfo() {
+        return $this->curlInfo;
+    }
+
+    /**
+     * @return array
+     */
+    public function getErrors() {
+        return $this->errors;
+    }
+
+    /**
+     * @return mixed|null
+     */
+    public function getLastError() {
+        if ($this->errors) {
+            return end($this->errors);
+        }
+        return null;
+    }
+}
---
+++ src/telemanpl/EpgParser.php
@@ -0,0 +1,321 @@
+<?php
+
+namespace telemanpl;
+
+/**
+ * All channels http://www.teleman.pl/moje-stacje
+ * Programs for channel and day http://www.teleman.pl/program-tv/stacje/Puls?date=2016-05-13&hour=-1 - channel Puls for 2016-05-13 - hour=-1 - за весь день
+ * If channel name consists of several words, url must be like http://www.teleman.pl/program-tv/stacje/Filmbox-Extra-HD
+ *
+ * Class EpgParser
+ * @property array $categories Channels categories(sections)
+ * @package telemanpl
+ */
+class EpgParser extends BaseEpgParser
+{
+    protected $categories = array();
+    protected $channels = array();
+    protected $programs = array();
+
+    protected $channelsPage = null;
+    protected $currentChannel = null;
+
+
+    public function loadChannels() {
+        $this->initCurl($this->config['channelsUrl'])->runCurl();
+        if ($this->curlError) {
+            $this->setError($this->curlError);
+            return false;
+        }
+        if ($this->curlInfo['http_code'] != '200' || strpos($this->curlInfo['content_type'], 'application/json') === false) {
+            $this->setError("http code is not OK or content is invalid " . $this->curlInfo['http_code'] . "/" . $this->curlInfo['content_type']);
+            return false;
+        }
+        $this->channelsPage = $this->curlResult;
+        return $this->channelsPage;
+    }
+
+    /**
+     * @param $page
+     * @return bool
+     */
+    public function parseChannels($page) {
+        if (!$page) {
+            $this->setError("No page content is set");
+            return false;
+        }
+        $dom = new \DOMDocument();
+        $dom->preserveWhiteSpace = false;
+        $dom->validateOnParse = true;
+        @$dom->loadHTML($page);
+        $form = $dom->getElementById("user-stations-form");
+        if (!$form) {
+            unset($dom);
+            $this->setError("No stations form");
+            return false;
+        }
+        $sections = $form->getElementsByTagName('div');
+        if (!$sections || !$sections->length) {
+            unset($dom);
+            $this->setError("No sections found for stations");
+            return false;
+        }
+        foreach ($sections as $section) {
+            /**
+             * @var \DOMElement $section
+             */
+            if (strpos($section->getAttribute('class'), 'section') === false) {
+                continue;
+            }
+            $categoryList = $section->getElementsByTagName('h2');
+            if (!$categoryList || !$categoryList->length) {
+                continue;
+            }
+            $category = $categoryList->item(0)->textContent;
+            $this->categories[] = $category;
+            $divContainer = $section->getElementsByTagName('div');
+            if (!$divContainer || !$divContainer->length) {
+                continue;
+            }
+            /**
+             * @var \DOMElement $container
+             */
+            $container = $divContainer->item(0);
+            $uls = $container->getElementsByTagName('ul');
+            if (!$uls || !$uls->length) {
+                continue;
+            }
+            $this->parseSectionUnsignedLists($uls, $category);
+
+        }
+
+        unset($dom);
+
+    }
+
+    /**
+     * @param \DOMNodeList $lists
+     * @param string $category
+     * @return bool|array
+     */
+    protected function parseSectionUnsignedLists($lists, $category) {
+        if (!$lists || !$lists->length) {
+            return false;
+        }
+        $channels = array();
+        foreach ($lists as $list) {
+            /**
+             * @var \DOMElement $list
+             */
+            $lis = $list->getElementsByTagName('li');
+            if (!$lis || !$lis->length) {
+                continue;
+            }
+            $channel = array();
+
+            foreach ($lis as $li) {
+                /**
+                 * @var \DOMElement $li
+                 */
+                $spans = $li->getElementsByTagName('span');
+                if (!$spans || !$spans->length) {
+                    continue;
+                }
+                $channelName = $spans->item(0)->textContent;
+                $inputs = $li->getElementsByTagName('input');
+                if (!$inputs || !$inputs->length) {
+                    continue;
+                }
+                $channelData = $inputs->item(0);
+                /**
+                 * @var \DOMElement $channelData
+                 */
+                $channelId = $channelData->getAttribute('value');
+                $channel['id'] = $channelId;
+                $channel['name'] = $channelName;
+                $channel['category'] = $category;
+                array_push($channels, $channel);
+                array_push($this->channels, $channel);
+            }
+        }
+        unset($lists);
+        return $channels;
+    }
+
+    /**
+     *
+     * @param string $day as Y-m-d
+     * @param string $channelName
+     * @return array|boolean
+     */
+    public function loadDay($day, $channelName) {
+        try {
+            $dayObject = new \DateTime($day);
+        } catch (\Exception $e) {
+            $this->setError($e->getMessage());
+            return false;
+        }
+        $this->currentChannel = $channelName;
+        $channelName = str_replace(" ", "-", trim($channelName));
+
+        $url = $this->config['baseUrl'] . $channelName . "?date=" . $dayObject->format('Y-m-d') . "&hour=-1";
+        $this->initCurl($url)->runCurl();
+        if ($this->curlError) {
+            $this->setError($this->curlError);
+            return false;
+        }
+        if ($this->curlInfo['http_code'] != '200' ) {
+            $this->setError("http code is not OK or content is invalid " . $this->curlInfo['http_code'] . "/" . $this->curlInfo['content_type']);
+            return false;
+        }
+        return $this->curlResult;
+    }
+
+    /**
+     * @param $page
+     * @return bool|array
+     */
+    public function parseDaySchedule($page) {
+        if (!$page) {
+            $this->setError("No page content is set");
+            return false;
+        }
+        $dom = new \DOMDocument();
+        $dom->preserveWhiteSpace = false;
+        $dom->validateOnParse = true;
+        @$dom->loadHTML($page);
+        $main = $dom->getElementById("stationListing");
+        if (!$main) {
+            unset($dom);
+            $this->setError("No main block");
+            return false;
+        }
+        $uls = $main->getElementsByTagName('ul');
+        if (!$uls || !$uls->length) {
+            return false;
+        }
+        foreach ($uls as $ul) {
+            /**
+             * @var \DOMElement $ul
+             */
+            if ($ul->getAttribute('class') == 'stationItems') {
+                $this->parseStationItems($ul->getElementsByTagName('li'));
+            }
+        }
+        return $this->programs;
+    }
+
+    /**
+     * @param \DOMNodeList $data
+     * @return bool
+     */
+    protected function parseStationItems($data) {
+        if (!$data || !$data->length) {
+            return false;
+        }
+        $programs = array();
+        foreach ($data as $li) {
+            $program = array();
+            /**
+             * @var \DOMElement $li
+             */
+            $id = $li->getAttribute('id');
+            if (strpos($id, 'prog') !== 0) {
+                continue;
+            }
+            $class = $li->getAttribute('class');
+            if (strpos($class, 'with-photo') !== false) {
+                $photoLinks = $li->getElementsByTagName('a');
+                if ($photoLinks && $photoLinks->length) {
+                    /**
+                     * @var \DOMElement $phLink
+                     */
+                    $phLink = $photoLinks->item(0);
+                    $phImgs = $phLink->getElementsByTagName('img');
+                    if ($phImgs && $phImgs->length) {
+                        /**
+                         * @var \DOMElement $img
+                         */
+                        $img = $phImgs->item(0);
+                        $program['img'] = $img->getAttribute('src');
+                    }
+                }
+            }
+
+            $divs = $li->getElementsByTagName('div');
+            if (!$divs || !$divs->length) {
+                continue;
+            }
+            $ems = $li->getElementsByTagName('em');
+            if (!$ems || !$ems->length) {
+                $this->setError("No start time found");
+                continue;
+            }
+            $startAt = $ems->item(0)->textContent;
+            $program['start'] = $startAt;
+            foreach ($divs as $d) {
+                /**
+                 * @var \DOMElement $d
+                 */
+                if ($d->getAttribute('class') == 'detail') {
+                    $links = $d->getElementsByTagName('a');
+                    if (!$links || !$links->length) {
+                        break;
+                    }
+                    /**
+                     * @var \DOMElement $link
+                     */
+                    $link = $links->item(0);
+                    $href = $link->getAttribute('href');
+                    $name = $link->textContent;
+                    $program['url'] = $href;
+                    $program['name'] = $name;
+                    break;
+                }
+
+            }
+
+            $paragraphs = $li->getElementsByTagName('p');
+            if ($paragraphs && $paragraphs->length) {
+                foreach ($paragraphs as $par) {
+                    /**
+                     * @var \DOMElement $par
+                     */
+                    if ($par->getAttribute('class') == 'genre') {
+                        $program['genre'] = $par->textContent;
+                    } elseif (!$par->getAttribute('class')) {
+                        $program['descr'] = $par->textContent;
+                    }
+                }
+            }
+            $program['channel'] = $this->currentChannel;
+            array_push($this->programs, $program);
+            array_push($programs, $program);
+        }
+
+        return $programs;
+    }
+
+    public function getProgramInfo($id) {
+
+    }
+
+    public function parseProgramData($json = array()) {
+
+    }
+
+    /**
+     * @return array
+     */
+    public function getChannels() {
+        return $this->channels;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCategories() {
+        return $this->categories;
+    }
+
+}
